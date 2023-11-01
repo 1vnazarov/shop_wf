c821bcde7ddd55022aa24112a5fe36872c8d796e
1vnazarov <115411574+1vnazarov@users.noreply.github.com>
2023-11-01 14:20:47 +0300

    Работа с файлами

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,49 @@ namespace check
         {
             InitializeComponent();
         }
+        string filePath = AppDomain.CurrentDomain.BaseDirectory + "/log.txt";
+        string readFile(string filePath)
+        {
+            try
+            {
+                // Проверяем существование файла
+                if (File.Exists(filePath))
+                {
+                    // Считываем содержимое файла
+                    return File.ReadAllText(filePath);
+                }
+                else
+                {
+                    MessageBox.Show("Файл не найден.");
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при чтении файла: " + ex.Message);
+                return null;
+            }
+        }
+
+        string fileWrite(string data)
+        {
+            string fullData = "";
+            if (data != null)
+            {
+                fullData = DateTime.Now.ToString("hh:mm:ss tt") + "\n" + data + "\n";
+                if (File.Exists(filePath)) fullData = File.ReadAllText(filePath) + fullData;
+            }
+            try
+            {
+                File.WriteAllText(filePath, fullData);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
         double full_price = 0;
         double full_valPercent = 0;
         double full = 0;
@@ -39,6 +82,7 @@ namespace check
             double nds = 0.13 * totalPrice;
             double totalTotalPrice = totalPrice - nds;
             textBox_info.Text += "Вы приобрели товар: " + textBox_name.Text + "\r\nПо цене: " + price + "\r\nСтоимость товара: " + onePrice + "\r\nВеличина скидки: " + percentVal + "\r\nСтоимость со скидкой: " + totalPrice + "\r\nНДС(13%): " + nds + "\r\nИтоговая цена за товар: " + totalTotalPrice + "\r\n";
+            fileWrite(textBox_info.Text);
             textBox_name.Text = "";
             textBox_percent.Text = "";
             textBox_price.Text = "";
@@ -64,6 +108,7 @@ namespace check
             textBox_percentInTotal.Text = "";
             textBox_inCassa.Text = "";
             textBox_info.Text = "";
+            fileWrite();
         }
     }
 }
